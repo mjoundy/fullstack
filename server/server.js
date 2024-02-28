38d00c68573f0ce3dc50f6a9cdcce7d143dfb687
mariem <joundy.mariem@gmail.com>
2024-02-28 12:57:30 +0100

    second commit

--- server/server.js
+++ server/server.js
@@ -3,11 +3,35 @@ const app = express();
 const port = 3002;
 const mongoose = require("mongoose");
 require("dotenv").config();
+const User = require("./models/Users");
+app.use(express.urlencoded({ extended: true }));
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
   res.send("Hello World!!");
 });
+app.get("/users", async (req, res) => {
 
+  const users = await UserModel.find();
+
+  res.json(users);
+  console.log("date save")
+});
+
+app.post("/createuser", (req, res) => {
+  const newUser = new User(req.body);
+  console.log(req.body)
+  newUser
+    .save()
+    .then((result) => {
+      console.log("data saved");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 mongoose
   .connect(
     `mongodb+srv://joundymariem:${process.env.MONGODB_PASSWORD}@cluster0.4m7n9cs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
